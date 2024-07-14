1b03b0bb01df526525d79e129a8242c8d03c81c7
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-07-14 20:31:59 +0530

    done

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -1,4 +1,4 @@
-const YourWeatherbtn = document.querySelector(".yourWeather");
+
 const temperatureStatus = document.querySelector(".tempStatus");
 const weatherstatus = document.querySelector(".weatherResult");
 const temptext = document.querySelector(".Temp");
@@ -19,30 +19,21 @@ const apiId = "5aac0747a4a9c46ace2f74c231775671";
 
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-function showMyTemp() {
-  temperatureStatus.style.display = "flex";
-  weatherstatus.style.display = "initial";
-  temptext.style.display = "initial";
-  weatherconditionImage.style.display = "flex";
-}
-function searchWeather() {
-  temperatureStatus.style.display = "none";
-  weatherstatus.style.display = "none";
-  temptext.style.display = "none";
-  weatherconditionImage.style.display = "none";
-}
-
 searchbtn.addEventListener("click", function () {
   showcityweather(cityInput.value);
 });
 
 async function showcityweather(city) {
   const response = await fetch(apiURL + city + `&appid=${apiId}`);
-  
   if (response.status == 404) {
     notfound.style.display = "initial";
+    temperatureStatus.style.display = "none";
+    weatherstatus.style.display = "none";
+    temptext.style.display = "none";
+    weatherconditionImage.style.display = "none";
   }
   else {
+   notfound.style.display = "none";
     var data = await response.json();
     temperatureStatus.style.display = "flex";
     weatherstatus.style.display = "initial";
@@ -53,7 +44,8 @@ async function showcityweather(city) {
     tempt.innerHTML = `${parseInt(data.main.temp)}°C `;
     wind.innerHTML = `${data.wind.speed}m/s`
     humidity.innerHTML = `${data.main.humidity}%`
-    cloud.innerHTML = `${data.clouds.all}%`
+    cloud.innerHTML = `${data.clouds.all}%`;
+
     if ((data.weather[0].main = "humidity")) {
       weatherimg.src = "Images/cloud.png";
     }
